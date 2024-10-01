af2393f2c3990c8b99d9371cf2813adb9c3c624f
agarwalmohit26 <161825186+agarwalmohit26@users.noreply.github.com>
2024-10-01 16:01:17 +0530

    updated

---
+++ day_1/krushna.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+"""
+Abbrivations:
+cbs = crime by state
+cbd = crime by district 
+"""
+
+cbs = pd.read_csv(".././datasets/crime_by_state_rt.csv")
+cbs
---
+++ day_1/rishav.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+"""
+Abbrivations:
+cbs = crime by state
+cbd = crime by district 
+"""
+
+cbd = pd.read_csv(".././datasets/crime_by_district_rt.csv")
+cbd
